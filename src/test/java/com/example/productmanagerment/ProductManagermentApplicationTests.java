dd24020093e9d6c7c73a5f5799de041fe5732032
--replace-all <loidv01071999@gmail.com>
2021-12-30 09:59:43 +0700

    initial commit

---
+++ src/test/java/com/example/productmanagerment/ProductManagermentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productmanagerment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagermentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
